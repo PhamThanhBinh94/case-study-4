806dc430acae8898694c07bf7ad280e0ccae8325
tuyentac95 <tuyentac95@gmail.com>
2020-10-05 14:12:02 +0700

    them model Product

--- src/main/java/com/codegym/model/Product.java
+++ src/main/java/com/codegym/model/Product.java
@@ -1,4 +1,23 @@
 package com.codegym.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "product")
+@Data
 public class Product {
+    @Id
+    @NotEmpty
+    public String id;
+    public String type;
+    public String name;
+    public String brand;
+    public int price;
+    public String image;
+    public int amount;
 }
