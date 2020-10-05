4e21d404a18eb9c8996d4242a7705e04a91e5bf5
tuyentac95 <tuyentac95@gmail.com>
2020-10-05 14:50:31 +0700

    tạo ProductRepository, ProductService, ProductServiceImpl, list.html, ProductController

--- src/main/java/com/codegym/repository/ProductRepository.java
+++ src/main/java/com/codegym/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.codegym.repository;
 
-public interface ProductRepository {
+import com.codegym.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, String> {
 }
