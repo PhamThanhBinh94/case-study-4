fbc9c1dd461f4633197af7ab93f92755f8359127
Nqvinh04 <nqvinh1995@gmail.com>
2020-10-05 17:15:31 +0700

    update CRUD product

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/products")
@@ -22,6 +23,44 @@ public class ProductController {
         Page<Product> products = productService.findAll(pageable);
         ModelAndView modelAndView = new ModelAndView("/product/list");
         modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateProduct(){
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("products", new Product());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveProduct(@ModelAttribute("product") Product product){
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("products", new Product());
+        return modelAndView;
+    }
+    
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditUser(@PathVariable String id){
+        Product product = productService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("product/edit");
+        modelAndView.addObject("products",product);
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView updateUser(@ModelAttribute("product") Product product) throws IOException {
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("product/edit");
+        modelAndView.addObject("products", new Product());
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable String id){
+        productService.deleteById(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/products");
         return modelAndView;
     }
 }
