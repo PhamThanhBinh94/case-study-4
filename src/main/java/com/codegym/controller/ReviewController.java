8e95d7983f78fe7aa4c5e2041ca88538fd2abf0c
tuyentac95 <tuyentac95@gmail.com>
2020-10-08 08:09:03 +0700

    sua tinh nang review

--- src/main/java/com/codegym/controller/ReviewController.java
+++ src/main/java/com/codegym/controller/ReviewController.java
@@ -1,8 +1,11 @@
 package com.codegym.controller;
 
+import com.codegym.model.Product;
 import com.codegym.model.Review;
 import com.codegym.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +24,14 @@ public class ReviewController {
     public List<Integer> getListRating(@RequestBody Review review){
         review.setDate(new Date());
         System.out.println(review);
-        reviewService.addReview(review);
+//        reviewService.addReview(review);
         return reviewService.getListRatingOfProductById(review.getProductId());
+    }
+
+    @RequestMapping(value = "/reviews/get", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Page<Review> getReviews(@RequestBody Product product, Pageable pageable){
+        return reviewService.findAllByProductId(product.getId(), pageable);
     }
 
 }
