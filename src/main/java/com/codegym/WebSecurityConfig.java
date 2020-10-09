98ecb6bcf027f56f78e555e5014451068feba042
Nqvinh04 <nqvinh1995@gmail.com>
2020-10-09 08:05:28 +0700

    update security

--- src/main/java/com/codegym/WebSecurityConfig.java
+++ src/main/java/com/codegym/WebSecurityConfig.java
@@ -1,4 +1,40 @@
 package com.codegym;
 
-public class WebSecurityConfig {
+import com.codegym.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    private CustomerSuccesHandler customerSuccesHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) accountService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/","/users/create/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/products","/products/*","/bill/**","/customers/**").hasRole("ADMIN")
+                .and()
+                .authorizeRequests().antMatchers("/users","/users/list", "/users/edit").hasRole("USERS")
+                .and().formLogin()
+                .successHandler(customerSuccesHandler)
+                .and().csrf()
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
 }
--- src/main/java/com/codegym/repository/AccountRepository.java
+++ src/main/java/com/codegym/repository/AccountRepository.java
@@ -2,7 +2,10 @@ package com.codegym.repository;
 
 import com.codegym.model.Account;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
     Account findByUsername(String username);
 }
